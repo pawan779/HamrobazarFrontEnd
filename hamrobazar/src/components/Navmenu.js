3ef0dd4b11aa2f9979fcf6237f9a1196572bac84
pawan779 <pawan.dharel77@gmail.com>
2020-01-19 11:10:56 +0545

    navbar created

---
+++ hamrobazar/src/components/Navmenu.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react'
+
+import {
+    Navbar,
+    Nav,
+    NavDropdown,
+    FormControl,
+    Form,
+    Button
+} from "react-bootstrap";
+
+export default class Navmenu extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar bg="light" expand="sm">
+                    <Navbar.Brand href="#home">Hamrobazar</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">About Us</Nav.Link>
+                            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+							<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+							<NavDropdown.Divider />
+							<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+						</NavDropdown> */}
+                        </Nav>
+
+                        <Button className="mr-sm-2 btn btn-dark">Login
+                        </Button>
+
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+        )
+    }
+}
